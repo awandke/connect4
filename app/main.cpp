c9972001851834528adf1a88b74ead5a048aac13
awandke <awandke@stu.naperville203.org>
2021-06-02 22:11:22 -0500

    added sfml library and reconfigured cmake with more directories and src library

--- app/main.cpp
+++ app/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "test_class.h"
 
 using std::cout;
 using std::endl;
---
+++ include/test_class.h
@@ -0,0 +1,20 @@
+//
+// Created by alanw on 6/2/2021.
+//
+
+#ifndef CONNECT4_TEST_CLASS_H
+#define CONNECT4_TEST_CLASS_H
+
+
+class test_class {
+public:
+  int getNum() {
+    return number;
+  }
+
+private:
+  int number = 1;
+};
+
+
+#endif //CONNECT4_TEST_CLASS_H
